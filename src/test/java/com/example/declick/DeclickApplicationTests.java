a7f64fb8b4356be74340e9449d216e278b74364e
marlenegohi <angemarlenegohi@gmail.com>
2025-03-18 22:14:47 +0100

    Initialisation du projet

---
+++ src/test/java/com/example/declick/DeclickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.declick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
